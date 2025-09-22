4bce0dbbf1388ddd37a90a61001e8b06e5e77b78
Antonova Kateryna <9123253@stud.op.edu.ua>
2025-09-22 13:16:22 +0300

    lab2 completed

--- src/main/java/ua/opnu/BankAccount.java
+++ src/main/java/ua/opnu/BankAccount.java
@@ -3,54 +3,51 @@ package ua.opnu;
 public class BankAccount {
     String name;
     double balance;
-    double transactionFee = 0.0;
+    double transactionFee;
 
-    BankAccount() {
-        this.name = "";
-        this.balance = 0.0;
-    }
-
-    void deposit(double amount) {
-        if (amount > 0) {
-            balance = balance + amount;
+        BankAccount() {
+            this.name = "";
+            this.balance = 0.0;
+            this.transactionFee = 0.0;
         }
-    }
 
-    double getBalance() {
-        return this.balance;
-    }
+        void deposit(double amount) {
+            if (amount > 0) {
+                balance = balance + amount;
+            }
+        }
 
-    boolean withdraw(double amount) {
-        if (amount <= 0) {
-            return false;
+        double getBalance() {
+            return this.balance;
         }
-        double totalDeduction = amount + transactionFee;
-        if (balance >= totalDeduction) {
-            balance = balance - totalDeduction;
-            return true;
-        } else {
-            return false;
-        }
-    }
 
-    boolean transfer(BankAccount receiver, double amount) {
-        if (amount <= 0) {
-            return false;
+        boolean withdraw(double amount) {
+            if (amount <= 0) {
+                return false;
+            }
+            double totalDeduction = amount + transactionFee;
+            if (balance >= totalDeduction) {
+                balance = balance - totalDeduction;
+                return true;
+            }
+                return false;
         }
-        double totalDeduction = amount + transactionFee;
-        if (balance >= totalDeduction) {
-            balance = balance - totalDeduction;
-            receiver.balance = receiver.balance + amount;
-            return true;
-        } else {
-            return false;
-        }
-    }
 
-    void setTransactionFee(double fee) {
-        if (fee >= 0) {
-            transactionFee = fee;
+        boolean transfer(BankAccount receiver, double amount) {
+            if (amount <= 0) return false;
+            double totalDeduction = amount + transactionFee;
+            if (balance >= totalDeduction) {
+                balance = balance - totalDeduction;
+                receiver.balance = receiver.balance + amount;
+                return true;
+            }
+                return false;
         }
-    }
 
-}
+        void setTransactionFee(double fee) {
+            if (fee >= 0) {
+                transactionFee = fee;
+            }
+        }
+
+    }
